d7ca283a1eea8bb4bebe728023df96c6f580b076
Loic <lemerlus.loic@gmail.com>
2015-07-26 18:19:54 +0100

    Modifications

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 library(dplyr)
-
+##1.Merges the training and the test sets to create one data set.
 ##Get the test data
 x_test<-read.table("UCI HAR Dataset/test/X_test.txt")
 y_test<-read.table("UCI HAR Dataset/test/y_test.txt")
@@ -32,12 +32,8 @@ Y<-NULL
 subject<-NULL
 
 
-##get the correspondance between activity label and name
-activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")
-## renaming the labels
-data<-mutate(data,Activities=activity_labels$V2[Activities])
-activity_labels<-NULL
 
+##2.Extracts only the measurements on the mean and standard deviation for each measurement. ##
 ## Get the name of the variables
 features<-read.table("UCI HAR Dataset/features.txt")
 ##Rename the columns
@@ -45,16 +41,31 @@ d<-data.frame(V1=1:2,V2=c("Subjects","Activities"))
 features <-rbind(d,features)
 names(data)<-features$V2
 features<-NULL
-
 ##Keep only the means and standard deviation
 for(x in names(data)){
   if (!grepl("mean()",x) && !grepl("std()",x) && !grepl("Activities",x) && !grepl("Subjects",x))
-    {
-      data[[x]]<- NULL
-    }
+  {
+    data[[x]]<- NULL
+  }
 }
 
-##rename columns
+
+##3. Uses descriptive activity names to name the activities in the data set ##
+
+##get the correspondance between activity label and name
+activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")
+## renaming the labels
+data<-mutate(data,Activities=activity_labels$V2[Activities])
+activity_labels<-NULL
+
+
+
+
+## 4. Appropriately labels the data set with descriptive variable names. ##
+
+
+
+##rename columns to be more explicite
 for(x in names(data)){
   if(!grepl("Activities",x) && !grepl("Subjects",x)){
     var = ""
@@ -93,12 +104,10 @@ for(x in names(data)){
     
     names(data)[names(data) == x]<-var
   }
-  
-  
 }  
 
-
-##Creating tiny data
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject. ###
+##Group by subject and activities
 data2<-aggregate(data,by=list(data$Subjects,data$Activities),FUN= mean,na.rm=TRUE)
 data2$Subjects <-NULL
 data2$Activities <-NULL
@@ -107,4 +116,3 @@ data2<-rename(data2,Subjects=Group.1,Activities = Group.2)
 
 ##export the table
 write.table(data2,"export.txt",row.names=FALSE)
-data2
